bd2e1b65c8a9df5a51699d9e0cfd6bd485a09363
camfisher <camfisher98@gmail.com>
2020-02-01 19:32:05 -0500

    Initial File Setup

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+//g++ main.cpp -Ilibdiscord/include -ldiscord -lboost_system -lcrypto -lssl -lcpprest -lz -lpthread -std=c++14 -o output_file
+
+#include "sleepy_discord/sleepy_discord.h"
+#include "websocketpp_websocket.h"
+
+class myClientClass : public SleepyDiscord::DiscordClient {
+public:
+    using SleepyDiscord::DiscordClient::DiscordClient;
+    void onMessage(SleepyDiscord::Message message) {
+        if (message.startsWith("whcg hello"))
+            sendMessage(message.channelID, "Hello " + message.author.username);
+    }
+};
+
+int main() {
+    myClientClass client("token", 2);
+    client.run();
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CafeBot
+
+# How to Install MS SQL Server
+https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-ubuntu?view=sql-server-ver15
