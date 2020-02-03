08fc8cacdc7325655b0fccc2c54063d594c837f3
camfisher <camfisher98@gmail.com>
2020-02-02 19:20:59 -0500

    example code

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
-//g++ main.cpp -Ilibdiscord/include -ldiscord -lboost_system -lcrypto -lssl -lcpprest -lz -lpthread -std=c++14 -o output_file
 
-#include "discord.h"
 
-using namespace discord;
+#include "websocketpp_websocket.h"
 
-int main() { // REMINDER: DO NOT PUSH TOKEN TO MASTER!!!!!
-  std::string token = "YOUR_TOKEN"; //Get Bot Token From: https://discordapp.com/developers/applications/
-  auto bot = Bot(token); // DON'T DO IT!!
-
-  bot->on_message([](MessageEvent& event) {
-    if (event.content() == "Ping!") {
-      event.respond("Pong!");
+class myClientClass : public SleepyDiscord::DiscordClient {
+public:
+    using SleepyDiscord::DiscordClient::DiscordClient;
+    void onMessage(SleepyDiscord::Message message) {
+        if (message.startsWith("whcg hello"))
+            sendMessage(message.channelID, "Hello " + message.author.username);
     }
-  });
+};
 
-  bot->run();
+int main() {
+    myClientClass client("token", 2);
+    client.run();
 }
